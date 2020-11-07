aa0044aacd65e2e3544363291f0f07e5be547fcd
ShorMeneses <62905042+ShorMeneses@users.noreply.github.com>
2020-11-07 00:04:28 +0000

    Pages added

--- public/index.js
+++ public/index.js
@@ -1,31 +1,80 @@
 $(function () {
+  var currPage = 1;
+  var pagIAmat = 1;
   const editor = ace.edit("editor");
   editor.setTheme("ace/theme/monokai");
-  editor.session.setMode("ace/mode/java");
-  editor.setOption("showPrintMargin", false)
+  editor.session.setMode("ace/mode/javascript");
+  editor.setOption("showPrintMargin", false);
+  editor.setOption("highlightActiveLine", true);
+  editor.getSession().setUseWorker(false);
   var socket = io();
+  var arrayText = [];
 
-  
-  $('div#editor').bind('input change paste', function() {
-    console.log("sent");
-    socket.emit('chat message', editor.getValue());
-  
-      return false;
+  setEventListener($('#newPage'));
+
+  $('div#editor').bind('input change paste', function () {
+    var str=("message "+pagIAmat);
+    console.log(str);
+    socket.emit(str, editor.getValue());
+    return false;
   });
 
-  document.querySelector('textarea').addEventListener("keyup",function (ev){
-    if (ev.keyCode === 46 || ev.keyCode === 8  ){
-      console.log("sent");
-      socket.emit('chat message', editor.getSession().getValue());
+  document.querySelector('textarea').addEventListener("keyup", function (ev) {
+    if (ev.keyCode === 46 || ev.keyCode === 8) {
+      socket.emit('message ' + pagIAmat, editor.getSession().getValue());
     }
   });
 
-    socket.on('chat message', function(msg){
-      console.log("recevive");
-      var x =editor.selection.getCursor().row;
-      var y = editor.selection.getCursor().column;
-      
-         editor.setValue(msg);
-         editor.selection.moveTo(x,y);
-     });
+  for (var i = 1; i < 11; i++) {
+    socket.on('message ' + i, function (msg) {
+      arrayText = msg;
+      setText(pagIAmat);
+    });
+  }
+
+  socket.on('start', function (msg) {
+    arrayText = msg;
+    setText(1);
+  })
+
+
+  function setText(i) {
+    console.log(arrayText.length);
+    var x = editor.selection.getCursor().row;
+    var y = editor.selection.getCursor().column;
+    editor.setValue(arrayText[i]);
+    editor.selection.moveTo(x, y);
+  }
+
+  function setEventListener(ele) {
+    ele.bind("click", function (e) {
+      if (currPage < 10) {
+        currPage++;
+        var num = parseInt(currPage);
+        pagIAmat = num;
+        var addPage = $("#newPage").parent().remove();
+        $("ul").append("<li class=\"active\"><a data-toggle=\"tab\" href=" + num + " aria-expanded=\"false\">Page " + num + "</a></li>");
+        if (!(num == 10)) {
+          $("ul").append("<li><a id=\"newPage\" data-toggle=\"tab\" href=\"11\">Show new page</a></li>");
+          setEventListener($("#newPage"));
+          $("ul li").removeClass();
+          $("ul li").last().prev().addClass("active");
+        } else {
+          addPage = undefined;
+          $("ul li").removeClass();
+          $("ul li").last().addClass("active");
+        }
+
+        setText(currPage);
+        $("ul li").bind('click', function (e) {
+          pagIAmat = $(this).children(":first").attr('href');
+          setText(pagIAmat);
+        })
+      } else {
+        alert("Máximo de 10");
+      }
+
+    })
+  }
+
 });
--- share.js
+++ share.js
@@ -1,5 +1,11 @@
-var app = require('express')();
+var express = require('express');
+var favicon = require('serve-favicon')
 var fs = require('fs');
+var path = require('path');
+var app= express();
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'images','favicon.ico')));
+
 var https = require('https').createServer({
     key: fs.readFileSync('./Certs/server.key'),
     cert: fs.readFileSync('./Certs/server.cert'),
@@ -7,65 +13,37 @@ var https = require('https').createServer({
     rejectUnauthorized: false
 },app);
 
+
+
+var arrayText = ['Welcome to page 0','Welcome to page 1','Welcome to page 2','Welcome to page 3','Welcome to page 4','Welcome to page 5',
+    'Welcome to page 6','Welcome to page 7','Welcome to page 8','Welcome to page 9','Welcome to page 10'];
+
+
 var io = require('socket.io')(https);
-
-var arrayText = [];
-
-
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
 
-app.get('/jquery-3.4.1.min.js', (req, res) => {
-    res.sendFile(__dirname + '/public/jquery-3.4.1.min.js');
-  });
-
-app.get('/socket.io.js', (req, res) => {
-    res.sendFile(__dirname + '/public/socket.io.js');
-  });
-
-app.get('/index.js', (req, res) => {
-    res.sendFile(__dirname + '/public/index.js');
-  });
-
-app.get('/index.css', (req, res) => {
-    res.sendFile(__dirname + '/public/index.css');
-  });
-
-app.get('/ace-builds/src-noconflict/ace.js', (req, res) => {
-  res.sendFile(__dirname + '/public/ace-builds/src-noconflict/ace.js');
-});
-
-app.get('/ace-builds/src-noconflict/mode-java.js', (req, res) => {
-  res.sendFile(__dirname + '/public/ace-builds/src-noconflict/mode-java.js');
-});
-
-app.get('/ace-builds/src-noconflict/theme-monokai.js', (req, res) => {
-  res.sendFile(__dirname + '/public/ace-builds/src-noconflict/theme-monokai.js');
-});
-
-
-
-
 io.on('connection', (socket) => {
     console.log('a user connected');
-    for (var i=0;i<arrayText.length;i++){
-        socket.emit('chat message',arrayText[i]);
-    }
+        socket.emit('start',arrayText);
+
     
     socket.on('disconnect', () => {
       console.log('user disconnected');
     });
 
-    socket.on('chat message', (msg) => {
-        console.log('message: ' + msg);
-        arrayText.push(msg);
-        io.emit('chat message',msg);
-      });
-
+    for (let i =1 ;i<11;i++){
+      str="message "+i;
+      socket.on(str, (msg) => {
+        console.log("message:"+msg);
+        arrayText[i]=msg;
+        io.emit(str,arrayText);
+    });
+    }
   });
 
-https.listen(8081, () => {
+https.listen(443, () => {
   console.log('listening on *:443');
 });
